af3e3ecb03530b9e13bb580c2b8f852645f3daa9
cristobal <csarome@gmail.com>
2018-05-05 17:23:45 -0500

    working

--- HW3.py
+++ HW3.py
@@ -22,6 +22,12 @@ class LexicalChain:
         self.holonyms=[]
         self.add(word,index)
         self.closed=False
+        #we keep record of how many words we add by
+        #different method. This is usefull to adjuts the
+        #threshold
+        self.bycomparison=0
+        self.bysimilarity=0
+
     def add(self,word,index):
         self.indexes.append(index)
         self.words.append(word)
@@ -39,93 +45,116 @@ class LexicalChain:
         cond6=word in self.holonyms
         
         #find similarity
-        
         if (cond1 or cond2 or cond3 or cond4 or cond5 or cond6):
             self.add(word,index)
+            self.bycomparison+=1
             return True
         else:
-            cond7=aux.findSimilarity(0.9,word,self.words)
-            if cond7:
+            if aux.findSimilarity(0.85,word,self.words):
                 self.add(word,index)
-                print("match!")
+                #print("match!")
+                self.bysimilarity+=1
                 return True
             return False
-            
-#    def tryAdd(word):
-        
-    
-
-textFile=open("texts/text_02.txt","r")
-text=textFile.read()
-textFile.close()
-textSent=nltk.sent_tokenize(text)
-textWords=nltk.word_tokenize(text)
-wordPos=nltk.pos_tag(textWords)
-
-nouns=[]
-index=0
-wordIndex=[]
-for item in wordPos:
-    if item[1][0]=="N":
-        word=item[0].lower()
-        if len(word)>1:
-            nouns.append([word,index])
-#        wordIndex.append(index)
-    index+=1
-    
-#lexicalChains=[]
-#lc1=LexicalChain(nouns[0][0],nouns[0][1])
-
-lexChains=[]
-wordsAdded=[]
-i=0 
-for initial in nouns:
-    word1=initial[0]
-    index1=initial[1]
-    i+=1
-    print(i)
-    if index1 not in wordsAdded:
-        lexChains.append(LexicalChain(word1,index1))
-        wordsAdded.append(index1)
-        #print(len(lexChains))
-        for lc in lexChains:
-            if not lc.closed:
-                               
-                for item in nouns:
-                    #i+=1
-                    #print([i,len(lc.words)])
-                    word2=item[0]
-                    index2=item[1]
-                    if index2 not in wordsAdded:
-                        added=lc.checkAdd(word2,index2) 
-                        if added: wordsAdded.append(index2)
-                        #print(["exi",word])
-                lc.closed=True
+    def getChain(self):
+        #we compute the word counts with a dictionary
+        #first we initialize to 0
+        chain={word: 0 for word in self.words}
+        #after that we count the word occurrence
+        for word in self.words:
+            chain[word]=chain[word]+1
+        return chain
                 
-print(len(lexChains))
+        
+def main():           
 
-for lc in lexChains[:20]: 
-    print(set(lc.words))
-for lc in lexChains[:10]: 
-    print(lc.indexes)
-for lc in lexChains:
-    a=len(lc.indexes)
-    if a>1: print([a,lc.words])
+    textFile=open("texts/text_01.txt","r")
+    text=textFile.read()
+    textFile.close()
+    textSent=nltk.sent_tokenize(text)
+    textWords=nltk.word_tokenize(text)
+    wordPos=nltk.pos_tag(textWords)
     
-for lc in lexChains:
-    print(len(lc.indexes))
+    nouns=[]
+    index=0
+    wordIndex=[]
+    for item in wordPos:
+        if item[1][0]=="N":
+            word=item[0].lower()
+            if len(word)>1:
+                nouns.append([word,index])
+    #        wordIndex.append(index)
+        index+=1
+        
+    #lexicalChains=[]
+    #lc1=LexicalChain(nouns[0][0],nouns[0][1])
+    
+    print("Obtaining Lexical Chains")
+    lexChains=[]
+    wordsAdded=[]
+    i=0 
+    for initial in nouns:
+        word1=initial[0]
+        index1=initial[1]
+        i+=1
+        print('.', end='')
+        if index1 not in wordsAdded:
+            lexChains.append(LexicalChain(word1,index1))
+            wordsAdded.append(index1)
+            #print(len(lexChains))
+            for lc in lexChains:
+                
+                if not lc.closed:
+                                   
+                    for item in nouns:
+                        #i+=1
+                        #print([i,len(lc.words)])
+                        word2=item[0]
+                        index2=item[1]
+                        if index2 not in wordsAdded:
+                            added=lc.checkAdd(word2,index2) 
+                            if added: wordsAdded.append(index2)
+                            #print(["exi",word])
+                    lc.closed=True
+    
+    i=0
+    for chain in lexChains:
+        print()
+        i+=1
+        c=chain.getChain()
+        print("chain "+str(i)+': ',end='')
+        for word in c:
+            print(word+"("+str(c[word])+"), ",end='')
+                    
+    
+        class Testing:
+            def test():
+                
+                print(len(lexChains))
+                
+                for lc in lexChains[:20]: 
+                    print(set(lc.words))
+                for lc in lexChains[:10]: 
+                    print(lc.indexes)
+                i=0
+                for lc in lexChains:
+                    a=len(lc.indexes)
+                    if a>1:
+                        i+=1
+                        print([a,lc.words])
+                print(i)
+                    
+                for lc in lexChains:
+                    print(len(lc.indexes))
+                for lc in lexChains:
+                    print([lc.bycomparison,lc.bysimilarity])
+                for lc in lexChains:
+                    print(lc.getChain())
 
 
-#
-#
-#    
-#len(lexChains)
-#lc1.indexes
-#lc
-#lc1.antonyms
-#lc1.synonyms
-#lc1.hypernyms
-#lc1.hyponyms
+if __name__ == "__main__":
+    main()
+
 
 
 
